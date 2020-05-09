be5aecb967feede28eb383b142537bcb2a6dfcc2
zhouhaitao <zht@acmtc.com>
2020-05-09 18:58:39 +0800

    logback 支持 commit

--- springboot/src/main/java/org/galaxy/controller/DemoController.java
+++ springboot/src/main/java/org/galaxy/controller/DemoController.java
@@ -2,6 +2,8 @@ package org.galaxy.controller;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.galaxy.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,19 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+    public static Logger logger = LoggerFactory.getLogger(DemoController.class);
 
     @RequestMapping(value = "/method1", method = RequestMethod.POST)
     @ResponseBody
-    public String method1(HttpServletRequest request, @RequestBody User user){
+    public String method1(HttpServletRequest request, @RequestBody User user) {
+        logger.info("call method1 ");
         return "success method1";
     }
 
     @RequestMapping(value = "/method2", method = RequestMethod.GET)
     @ResponseBody
     public String method2(HttpServletRequest request, @RequestBody User user){
+        logger.info("call method2 ");
         return "success method2";
     }
 
